2d546fe09dd0b052c75f7046711351d44fd58609
Joel Joseph Roy <joelroy@meta.com>
2025-06-11 13:16:29 -0700

    Search component is now functional.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import MovieModal from "./Components/MovieModal";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredMovies, setFilteredMovies] = useState(null);
   const [openModal, setOpenModal] = useState(false);
   const [searchInput, setSearchInput] = useState("");
   const [page, setPage] = useState(1);
@@ -15,6 +17,7 @@ function App() {
   const fetchMovies = async () => {
     const apikey = import.meta.env.VITE_API_KEY;
     const URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apikey}&page=${page}`;
+    const searchURL = `https://api.themoviedb.org/3/search/movie?api_key=${apikey}&query=${searchInput}`;
 
     try {
       const response = await fetch(URL, {
@@ -31,6 +34,27 @@ function App() {
       console.log("Ran into an error: ", error);
     }
   };
+
+  const searchMovies = async () => {
+    const apikey = import.meta.env.VITE_API_KEY;
+    const searchURL = `https://api.themoviedb.org/3/search/movie?api_key=${apikey}&query=${searchInput}`;
+
+    try {
+      const response = await fetch(searchURL, {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+        },
+      });
+
+      const data = await response.json();
+      console.log(data.results);
+      setFilteredMovies(data.results || []);
+    } catch (error) {
+      console.log("Ran into an error: ", error);
+    }
+  };
+
   // A hook used to call fetchMovies when "page" changes
   useEffect(() => {
     fetchMovies();
@@ -44,20 +68,26 @@ function App() {
   // Handler for search inputs
   const searchMoviesHandler = (event) => {
     const search = event.target.value.toLowerCase();
+    setSearchQuery(search);
     setSearchInput(search);
+    searchMovies();
   };
 
   // Then filter it based on inputs
-  const filteredMovies = movies.filter((movie) => {
-    return movie.title.toLowerCase().includes(searchInput.toLowerCase());
-  });
+
+  // const filteredLocalMovies = movies.filter((movie) => {
+  //   return movie.title.toLowerCase().includes(searchInput.toLowerCase());
+  // });
+
+
+  // const searchedMovies = searchMovies(searchInput);
 
   // Needed to render the movies
-  const renderMovies = () => {
-    console.log("hello");
-    if (showMovies) return <MovieList movies={filteredMovies} />;
-    else return null;
-  };
+  // const renderMovies = () => {
+  //   console.log("hello");
+  //   if (showMovies) return <MovieList movies={filteredMovies} />;
+  //   else return null;
+  // };
 
   return (
     <div className="App">
@@ -68,9 +98,11 @@ function App() {
         type="search"
         placeholder="Search Movies"
         onChange={searchMoviesHandler}
+        value={searchInput}
       />
+      <button onClick={() => console.log("test")}>Submit</button>
       <MovieList
-        movies={movies}
+        movies={filteredMovies ? filteredMovies : movies}
         openModal={openModal}
         setOpenModal={setOpenModal}
       />
